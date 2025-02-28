4d869b55cbd5bb5312dd8e2a925bcfc5f2576e54
Christopher Fujino <christopherfujino@gmail.com>
2025-02-27 21:49:27 -0800

    fix build

--- lib/render.mli
+++ lib/render.mli
@@ -7,6 +7,11 @@ type constraints = {
 
 type widget = Text of string | Container of { border : int; child : widget }
 type size
-type render_target
+type render_target = {
+  size : size;
+  children : render_target list;
+  render : int -> int -> unit;
+}
+
 
 val layout : widget -> constraints -> render_target
